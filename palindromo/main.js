e2b9fbec02cbab3def1e236c56aa1c7e851f45ed
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-06 18:10:04 +0100

    esercizio sul palindromo non ancora completo (esperimenti commentati)

---
+++ palindromo/main.js
@@ -0,0 +1,27 @@
+//per prima cosa otteniamo una parola dall'utente
+let word = prompt("inserisci la parola che vuoi verificare :");
+console.log(word);
+
+//stabiliamo un ciclo for che ci stampi il nostro array di lettere
+for (let i = 0; i < word.length; i++) {
+    let lettere = word[i];
+    console.log(lettere)
+    
+}
+let lettere = word.split("");
+console.log(lettere);
+//for( let i = word.length; i >= 0; i--){         (questo era un ciclo for che riscriveva le parole al contrario)
+    //let verifica = word[i];
+   // console.log(verifica)}
+
+let max = word.length
+let min = i = 0
+
+let verifica = word.split("max, min");
+console.log(verifica);
+
+
+
+
+
+
